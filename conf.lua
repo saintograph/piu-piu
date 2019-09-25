f2e848caddabda5560bb1a9274f2d85a2976caf0
saintograph <winfredselwyn@gmail.com>
2019-09-25 22:40:52 +0800

    Added bounds and up/down movement.

--- conf.lua
+++ conf.lua
@@ -2,4 +2,6 @@ function love.conf(t)
   t.window.width = 1024
   t.window.height = 768
   t.window.title = "Piu piu"
+  t.modules.joystick = false
+  t.modules.physics = false
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
 debug = true
 
 -- Objects
+height = love.graphics.getHeight()
+width = love.graphics.getWidth()
 player = {
   img = nil,
   x = 100,
   y = 100,
-  speed = 150
+  speed = 350
 }
 
 
@@ -22,8 +24,18 @@ function love.update(dt)
     if player.x > 0 then
       player.x = player.x - (player.speed * dt)
     end
-	elseif love.keyboard.isDown('right','d') then
-		player.x = player.x + (player.speed * dt)
+  elseif love.keyboard.isDown('right','d') then
+    if player.x < width - 107 then
+      player.x = player.x + (player.speed * dt)
+    end
+  elseif love.keyboard.isDown('down','s') then
+    if player.y < (height - 93) then
+      player.y = player.y + (player.speed * dt)
+    end
+  elseif love.keyboard.isDown('up','w') then
+    if player.y > 0 then
+      player.y = player.y - (player.speed * dt)
+    end
 	end
 end
 
